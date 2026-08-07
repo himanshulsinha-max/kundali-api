ecd24e098dba92ad791767a54c0477d057bd6415
himanshulsinha-max <himanshulsinha@gmail.com>
2026-08-08 01:33:53 +0530

    Update dasha.py

--- dasha.py
+++ dasha.py
@@ -1,25 +1,34 @@
+from datetime import datetime, timedelta
+
 DASHA_ORDER = [
-    ("Ketu", 7),
-    ("Venus", 20),
-    ("Sun", 6),
-    ("Moon", 10),
-    ("Mars", 7),
-    ("Rahu", 18),
-    ("Jupiter", 16),
-    ("Saturn", 19),
-    ("Mercury", 17)
+    ("Ketu",7),
+    ("Venus",20),
+    ("Sun",6),
+    ("Moon",10),
+    ("Mars",7),
+    ("Rahu",18),
+    ("Jupiter",16),
+    ("Saturn",19),
+    ("Mercury",17)
 ]
 
 
-def get_mahadasha(moon_nakshatra):
+def get_mahadasha(moon_info, balance):
 
-    lord = moon_nakshatra["lord"]
+    lord = moon_info["lord"]
 
-    for planet, years in DASHA_ORDER:
+    years = 0
+
+    for planet, duration in DASHA_ORDER:
+
         if planet == lord:
-            return {
-                "current_mahadasha": planet,
-                "duration_years": years
-            }
+            years = duration
+            break
 
-    return None
+    remaining_years = years * balance
+
+    return {
+        "lord": lord,
+        "total_years": years,
+        "remaining_years": round(remaining_years,2)
+    }
