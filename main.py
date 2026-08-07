887b69778e1e09352f1ff8dc594c185ed962c3f4
himanshulsinha-max <himanshulsinha@gmail.com>
2026-08-07 11:02:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -27,13 +27,9 @@ def calculate_kundli(data: KundliRequest):
 
     julian_day = swe.julday(year, month, day, hour)
 
-    sun_pos = swe.calc_ut(julian_day, swe.SUN, swe.FLG_SIDEREAL)[0][0]
-    moon_pos = swe.calc_ut(julian_day, swe.MOON, swe.FLG_SIDEREAL)[0][0]
+   planets = calculate_planets(julian_day)
 
-    return {
-        "status": "success",
-        "sun_degree": round(sun_pos, 2),
-        "moon_degree": round(moon_pos, 2),
-        "sun_sign_id": int(sun_pos // 30) + 1,
-        "moon_sign_id": int(moon_pos // 30) + 1
-    }
+return {
+    "status": "Success",
+    "planets": planets
+}
