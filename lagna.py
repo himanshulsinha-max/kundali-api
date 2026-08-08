2c1cffd4296617e8277ce91f8f6a5dfff5f73cba
himanshulsinha-max <himanshulsinha@gmail.com>
2026-08-08 00:42:57 -0700

    Update lagna.py

--- lagna.py
+++ lagna.py
@@ -1,5 +1,6 @@
 import swisseph as swe
 
+
 SIGNS = [
     "Aries",
     "Taurus",
@@ -15,10 +16,44 @@ SIGNS = [
     "Pisces"
 ]
 
+
+def normalize_longitude(longitude):
+    """
+    Keep longitude between 0 and 360 degrees.
+    """
+    return longitude % 360.0
+
+
+def get_sign_index(longitude):
+    """
+    Return zodiac sign index from sidereal longitude.
+    """
+    longitude = normalize_longitude(longitude)
+
+    return int(longitude // 30)
+
+
 def get_sign_name(longitude):
-    return SIGNS[int(longitude // 30)]
+    """
+    Return zodiac sign name.
+    """
+    return SIGNS[get_sign_index(longitude)]
+
+
+def get_degree_in_sign(longitude):
+    """
+    Return degree within the zodiac sign.
+    """
+    longitude = normalize_longitude(longitude)
+
+    return longitude % 30.0
+
 
 def calculate_lagna(julian_day, latitude, longitude):
+
+    # We only need the Ascendant.
+    # D1 houses will be calculated separately
+    # using Whole-Sign methodology.
 
     houses, ascmc = swe.houses_ex(
         julian_day,
@@ -28,9 +63,11 @@ def calculate_lagna(julian_day, latitude, longitude):
         swe.FLG_SIDEREAL
     )
 
-    asc = ascmc[0]
+    asc = normalize_longitude(ascmc[0])
 
     return {
-        "longitude": round(asc, 2),
-        "sign": get_sign_name(asc)
+        "longitude": round(asc, 8),
+        "sign": get_sign_name(asc),
+        "sign_index": get_sign_index(asc),
+        "degree_in_sign": round(get_degree_in_sign(asc), 8)
     }
